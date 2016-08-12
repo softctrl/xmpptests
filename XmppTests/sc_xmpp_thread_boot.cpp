28305bf9005c45e46912368e5803cdbfe019f683
timoshenko <carlostimoshenkorodrigueslopes@gmail.com>
2016-08-12 08:59:19 -0300

    Just put the licence document header.

--- XmppTests/sc_xmpp_thread_boot.cpp
+++ XmppTests/sc_xmpp_thread_boot.cpp
@@ -1,3 +1,25 @@
+/*
+ *
+ * Author:
+ *  Carlos Timoshenko
+ *  carlostimoshenkorodrigueslopes@gmail.com
+ *
+ * Source:
+ *  https://github.com/softctrl/xmpptests
+ *
+ * This file is a part of XmppTests project.
+ *
+ * This project is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
 #include "sc_xmpp_thread_boot.h"
 #include <sc_xmpp_client_boot.h>
 #include <qxmpp/QXmppConfiguration.h>
